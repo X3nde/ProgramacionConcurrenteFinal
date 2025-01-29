4d3713dc657ff87645f335f200d71d8af69cf9d5
Xende <xende.rodriguez@hotmail.com>
2025-01-29 23:01:54 +0100

    Base

---
+++ src/main/java/com/progcon/programacionconcurrentefinal/ProgramacionConcurrenteFinalApplication.java
@@ -0,0 +1,13 @@
+package com.progcon.programacionconcurrentefinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramacionConcurrenteFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgramacionConcurrenteFinalApplication.class, args);
+    }
+
+}
